4a1f6dc12cd3ec39bc5e829badccd663ee9e3a77
maksc <maks.cieslak13@gmail.com>
2023-01-07 18:20:23 +0100

    initial commit

--- Table.java
+++ Table.java
@@ -1,3 +1,13 @@
-public class Table {
+import java.util.ArrayList;
+import java.util.Map;
 
+public class Table {
+    String name;
+    ArrayList<String> colNames;
+    ArrayList<Map<String, String>> rows;
+
+    public Table(String name, ArrayList<String> colNames) {
+        this.name = name;
+        this.colNames = colNames;
+    }
 }
