cc7ef4d8128c526c2c454814e7a7bd461c2e1d1d
maksc <maks.cieslak13@gmail.com>
2023-01-08 19:52:11 +0100

    commit

--- DatabaseProgram.java
+++ DatabaseProgram.java
@@ -1,3 +1,40 @@
-public class DatabaseProgram {
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Scanner;
 
+import static java.lang.System.exit;
+
+public class DatabaseProgram {
+    public static void main(String[] args) {
+        System.out.println("Do you want to create table?\n1 - Yes\n2 - No");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.nextLine();
+
+        if (Objects.equals(answer, "2")) {
+            System.out.println("Nauuuuura");
+            exit(0);
+        } else {
+            System.out.println("Enter name of table:");
+            String name = scanner.nextLine();
+            System.out.println("Enter columns:");
+            String colString = scanner.nextLine();
+            String[] cols = colString.split(" ");
+            Table table = new Table(name, cols);
+            System.out.println("Input values:");
+            String row1 = scanner.nextLine();
+            String[] values = row1.split(" ");
+            table.insert(values);
+            table.print();
+            try {
+                table.saveTable();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
 }
--- Table.java
+++ Table.java
@@ -1,13 +1,62 @@
+import java.io.BufferedWriter;
+import java.io.File;
 import java.util.ArrayList;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class Table {
     String name;
     ArrayList<String> colNames;
-    ArrayList<Map<String, String>> rows;
+    ArrayList<HashMap<String, String>> rows = new ArrayList<>();
 
-    public Table(String name, ArrayList<String> colNames) {
+    public Table(String name, String[] cols) {
         this.name = name;
-        this.colNames = colNames;
+        colNames = new ArrayList<String>(Arrays.asList(cols));
+    }
+
+    public void saveTable() throws IOException {
+        // not working :c
+        String fileName = name + ".txt";
+        File file = new File(fileName);
+        file.createNewFile();
+        BufferedWriter fileWriter = new BufferedWriter(new FileWriter(file));
+        for (String s : colNames) {
+            fileWriter.write(s);
+            fileWriter.write("|");
+        }
+        fileWriter.write("\n");
+        for (HashMap<String, String> m : rows) {
+            for (String s : colNames) {
+                fileWriter.write(m.get(s));
+                fileWriter.write("|");
+            }
+            fileWriter.write("\n");
+        }
+    }
+
+    public void insert(String[] values) {
+        HashMap<String, String> row = new HashMap<String, String>();
+
+        for (int i = 0; i < colNames.size(); i++) {
+            row.put(colNames.get(i), values[i]);
+        }
+        rows.add(row);
+    }
+
+    public void print() {
+        System.out.print("| ");
+        for (String col : colNames) {
+            System.out.print(col + " | ");
+        }
+        System.out.println();
+        for (HashMap<String, String> m : rows) {
+            System.out.print("| ");
+            for (String col : colNames) {
+                System.out.print(m.get(col) + " | ");
+            }
+            System.out.println();
+        }
     }
 }
